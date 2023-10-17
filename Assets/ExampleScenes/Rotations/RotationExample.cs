2baea4cf94a0b02e308da1827e928034e7fb1164
Sean Piche <spiche91@academic.rrc.ca>
2023-10-17 11:42:18 -0500

    Added angle axis and quaternion multiplication

--- Assets/ExampleScenes/Rotations/RotationExample.cs
+++ Assets/ExampleScenes/Rotations/RotationExample.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// Create by Sean Piche and Derek Baert 10/17/2023
 /// </summary>
-public enum RotationModeExample { RotateTowardsTarget, SnapToAngle, QuaternionMultiplication, QuaternionSetLookRotation, QuaternionSetFromToRotation }
+public enum RotationModeExample { RotateTowardsTarget, SnapToAngle, QuaternionMultiplication, AngleAxis, QuaternionSetFromToRotation }
 public class RotationExample : MonoBehaviour
 {
     [SerializeField]
@@ -21,7 +21,10 @@ public class RotationExample : MonoBehaviour
     private Quaternion startRotation;
 
     private float timeFromStart;
-    // Start is called before the first frame update
+
+    /// <summary>
+    /// Sets the initial startRotation
+    /// </summary>
     void Start()
     {
         startRotation = this.transform.rotation;
@@ -42,9 +45,9 @@ public class RotationExample : MonoBehaviour
         {
             QuaternionMultiplication();
         }
-        else if (rotationMode == RotationModeExample.QuaternionSetLookRotation) 
+        else if (rotationMode == RotationModeExample.AngleAxis) 
         {
-            
+            AngleAxis();
         }
         else if (rotationMode == RotationModeExample.QuaternionSetFromToRotation) 
         {
@@ -52,7 +55,10 @@ public class RotationExample : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// Rotates overtime to the target transform
+    /// </summary>
+    /// <param name="delta">Time.DeltaTime</param>
     private void RotateTowardsTarget(float delta)
     {
         if(targetRotation != this.transform.rotation.eulerAngles)
@@ -68,13 +74,47 @@ public class RotationExample : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Snaps to the target rotation angle
+    /// </summary>
     private void SnapToRotation()
     {
+        Debug.Log("Snapping to Rotation");
         this.transform.rotation = Quaternion.Euler(targetRotation);
     }
 
+    /// <summary>
+    /// Rotates by the target rotation
+    /// </summary>
     private void QuaternionMultiplication()
     {
+        Debug.Log("Multiplying Quaternions");
         this.transform.rotation = startRotation * Quaternion.Euler(targetRotation);
     }
+
+    /// <summary>
+    /// Points towards the target using angle axis
+    /// </summary>
+    private void AngleAxis()
+    {
+        Vector3 targetLookVector = target.transform.position - transform.position;
+        Debug.DrawRay(transform.position, targetLookVector);
+        float zRotation = Vector2.SignedAngle(Vector3.up, targetLookVector);
+        print(zRotation);
+        transform.rotation = Quaternion.AngleAxis(zRotation, Vector3.forward);
+    }
+
+    /*
+    private void QuaternionSetLookRotation()
+    {
+        Debug.Log("Setting look rotation");
+        Debug.Log(target.transform.position);
+        Vector3 targetLookVector = target.transform.position - transform.position;
+        Quaternion test = Quaternion.identity;
+        test.SetLookRotation(targetLookVector, Vector3.left);
+        this.transform.rotation = test;
+    }
+    */
+
+
 }
