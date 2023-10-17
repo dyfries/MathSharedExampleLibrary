b7044ecc8d7cf2fa88eb13ecee7c6a9d0362c989
Nat <nmogk@academic.rrc.ca>
2023-10-17 11:13:02 -0500

    Added other functions to lerp by

--- Assets/ExampleScenes/Easing/MathFunctionLerp/MathLerpOverTimeExample.cs
+++ Assets/ExampleScenes/Easing/MathFunctionLerp/MathLerpOverTimeExample.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CurveMode { Linear, Logarithmic, Exponential, SCurve }
+
 public class MathLerpOverTimeExample : MonoBehaviour
 {
     private float currentTimer = 0;
@@ -13,6 +15,8 @@ public class MathLerpOverTimeExample : MonoBehaviour
     private bool isCurrentlyLerping = false;
     private Vector3 startPosition;
     private Vector3 endPosition; // could do this different ways, I chose V3. 
+
+    [SerializeField] private CurveMode curve;
 
     // Update is called once per frame
     void Update()
@@ -49,9 +53,27 @@ public class MathLerpOverTimeExample : MonoBehaviour
             {
                 float proportionFinished = currentTimer / lerpTime; // scales the time range between 0 to 1
 
-                // sigmoid function : y = (k / 1 - euler's number ^ (-b * x))
-                // Mathf.Exp(1) is euler's number (2.71...)
-                float rate = 1 / (1 + Mathf.Pow(Mathf.Exp(1), -10 * proportionFinished + 5));
+                Debug.Log(proportionFinished);
+
+                float rate = 0;
+
+                switch (curve)
+                {
+                    case CurveMode.Linear:
+                        rate = proportionFinished;
+                        break;
+                    case CurveMode.Exponential:
+                        rate = Mathf.Pow(proportionFinished, 2);
+                        break;
+                    case CurveMode.Logarithmic:
+                        rate = Mathf.Sqrt(proportionFinished);
+                        break;
+                    case CurveMode.SCurve:
+                        // sigmoid function : y = (k / 1 - euler's number ^ (-b * x))
+                        // Mathf.Exp(1) is euler's number (2.71...)
+                        rate = 1 / (1 + Mathf.Pow(Mathf.Exp(1), -10 * proportionFinished + 5));
+                        break;
+                }
 
                 // iterate through
                 transform.position = Vector3.Lerp(startPosition, endPosition, rate);
